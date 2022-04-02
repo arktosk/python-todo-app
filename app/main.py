3e7cf644a240b19306010efb8deab1e2e87b37c8
Arkadiusz Krauzowicz <arktos.k@gmail.com>
2022-04-02 17:15:38 +0200

    Create router for user endpoints

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 
-from . import crud, models, schemas, settings
+from . import crud, schemas, settings
 from .security import create_access_token
-from .dependencies import get_db, get_current_user
-from .routers import todos
+from .dependencies import get_db
+from .routers import todos, users
 
 
 app = FastAPI()
@@ -31,32 +31,5 @@ async def login_for_access_token(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-
-@app.post("/users/me", response_model=schemas.User)
-def current_user(user: models.User = Depends(get_current_user)):
-    return user
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
+app.include_router(users.router)
 app.include_router(todos.router)
